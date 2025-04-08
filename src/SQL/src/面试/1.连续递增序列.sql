63027cf96465ee454b0668c91880fc2c399b8c39
Nobi-Nobita5 <419330259@qq.com>
2025-04-08 17:54:12 +0800

    力扣提交

--- src/SQL/src/面试/1.连续递增序列.sql
+++ src/SQL/src/面试/1.连续递增序列.sql
@@ -2,16 +2,16 @@
 拼多多：
 sql题目：输出表中num字段最长的连续递增序列。
 表t如下：
-id num
-1		2
-2		3
-3		4
-4		3
-5		4
-6		5
-7		6
-8		4
-9		5
+id     num   isNewSeq   groupId
+1		2     1           1
+2		3     0           1
+3		4     0           1
+4		3     1           2
+5		4     0           2
+6		5     0           2
+7		6     0           2
+8		4     1           3
+9		5     0           3
 应该输出：3 4 5 6
 写出这个sql
  */
@@ -21,6 +21,7 @@ WITH
         SELECT
             t.*,
             IF(t.num > LAG(t.num) OVER (ORDER BY t.id), 0, 1) AS isNewSeq
+            /*递增就标0，后面直接按顺序sum就可以得到分组*/
         FROM t
     ),
     GroupQuery AS (
